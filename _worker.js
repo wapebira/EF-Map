eb4c9ab69d984a8f59c5be549eae1b8224c48126
Michael Davis <michael.davis.home@gmail.com>
2025-09-07 09:31:16 +0100

    Cloudflare Pages: switch to _worker.js + clean wrangler config

---
+++ _worker.js
@@ -0,0 +1,5 @@
+// (Moved from worker.js) Cloudflare Pages Edge Worker – Full stats + shares + short URLs
+// See decision log 2025-09-07 entry for details.
+export * from './worker.js';
+import mod from './worker.js';
+export default mod;
